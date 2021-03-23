c694173161e1ad25f8d6f473610930b455985f42
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-23 00:17:57 -0700

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+    entry: './script.js',
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'transpiled.js'
+    },
+    module: {
+        loaders: [
+            {
+            test: /\.js$/,
+            loader: 'babel-loader', 
+            exclude: /node_modules/, 
+            query: {
+                presets: ['es2015', 'react']
+
+                } 
+            }
+        ]
+    }
+}
