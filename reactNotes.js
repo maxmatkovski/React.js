ca7da50f8e012dea4db1b9110d97fbe9e2263ebd
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-25 22:19:43 -0700

    Update reactNotes.js

--- reactNotes.js
+++ reactNotes.js
@@ -3,3 +3,21 @@
 const colors = ['red','green','blue'];
 const items = colors.map(color => `<li>${color}</li>`);
 console.log(items);
+
+// Object Destructuring
+
+  const address = {
+      street: '',
+      city: '',
+      country: '',
+  };
+
+  const street = address.street;
+  const city = address.city;
+  const country = adress.country;
+
+  // Destructuring Syntax
+  const {street, city, country} = address;
+
+  // Saves constant in new constant 'st';
+  const {street: st} = address;
