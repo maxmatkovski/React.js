ab1a75248f38aba35abfa93cbb73f59756b4c4f8
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-03-28 23:34:44 -0700

    Create classesAndInheritance.js

---
+++ classesAndInheritance.js
@@ -0,0 +1,27 @@
+ // Inheritance
+
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+
+    walk() {
+        console.log("walk");
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, degree){
+        super(name);
+        this.degree = degree;
+
+    }
+
+
+    teach(){
+        console.log('teach');
+    }
+}
+
+const teacher = new Teacher('Max', 'MSc');
+teacher.
